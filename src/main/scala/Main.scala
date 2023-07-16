36169b4b93e75e9a8564fcbf08752350a1aec3b9
Turdyo <martin.gayet@efrei.net>
2023-07-16 19:19:57 +0200

    create endpoints for reading matches + tests

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,6 @@ package mlb
 import zio._
 import zio.jdbc._
 import zio.http._
-import zio.stream.ZStream
 
 object MlbApi extends ZIOAppDefault {
 
@@ -21,44 +20,56 @@ object MlbApi extends ZIOAppDefault {
       props = properties
     )
 
-  val endpoints: App[ZConnectionPool] =
-    Http
-      .collectZIO[Request] {
-        case Method.GET -> Root => ZIO.from(Response.json("""{"response": "API works !"}"""))
-        case Method.GET -> Root / "init" => {
-          ZIO.from(Response.json("""{"response": "database initialised !"}"""))
-        }
-        case Method.GET -> Root / "season-playoff" => {
-          for{
-            result <- readAll
-            response <- ZIO.from(chunkOfTwoToJson(result))
-          } yield response
-        }
-
-        case Method.GET -> Root / "first-score" => {
-          for {
-            score <- readScore
-            response <- ZIO.from(optionToJson(score))
-          } yield response
-        }
-
-        case Method.GET -> Root / "predict" / "elo" / gameId => {
-          for {
-            _ <- Console.printLine(s"Prediction by elo for game: ${gameId}")
-            data <- predictEloGame(gameId)
-            response <- ZIO.from(optionToJson(data))
-          } yield response
-        }
-
-        case Method.GET -> Root / "predict" / "rating" / gameId => {
-          for {
-            _ <- Console.printLine(s"Prediction by rating for game: ${gameId}")
-            data <- predictRatingGame(gameId)
-            response <- ZIO.from(optionToJson(data))
-          } yield response
-        }
+  val endpoints: App[ZConnectionPool] = Http
+    .collectZIO[Request] {
+      case Method.GET -> Root => ZIO.from(Response.json("""{"response": "API works !"}"""))
+      case Method.GET -> Root / "init" => {
+        ZIO.from(Response.json("""{"response": "database initialised !"}"""))
       }
-      .withDefaultErrorResponse
+      // case Method.GET -> Root / "season-playoff" =>
+      //   for {
+      //     result <- readAll
+      //     response <- ZIO.from(chunkOfTwoToJson(result))
+      //   } yield response
+
+      case Method.GET -> Root / "first-score" =>
+        for {
+          score <- readScore
+          response <- ZIO.from(optionToJson(score))
+        } yield response
+
+      case Method.GET -> Root / "predict" / "elo" / gameId =>
+        for {
+          _ <- Console.printLine(s"Prediction by elo for game: ${gameId}")
+          data <- predictEloGame(gameId)
+          response <- ZIO.from(optionToJson(data))
+        } yield response
+
+      case Method.GET -> Root / "predict" / "rating" / gameId =>
+        for {
+          _ <- Console.printLine(s"Prediction by rating for game: ${gameId}")
+          data <- predictRatingGame(gameId)
+          response <- ZIO.from(optionToJson(data))
+        } yield response
+
+      case Method.GET -> Root / "matches" => for {
+        data <- readAll
+        response <- ZIO.from(matchChunkToJsonReponse(data))
+      } yield response
+
+      case Method.GET -> Root / "matches" / "season" / season => for {
+        data <- readBySeason(season = season)
+        response <- ZIO.from(matchChunkToJsonReponse(data))
+      } yield response
+
+      case Method.GET -> Root / "matches" / match_id => for {
+        data <- readMatch(match_id)
+        response <- ZIO.from(matchOptionToJsonReponse(data))
+      } yield response
+      
+
+    }
+    .withDefaultErrorResponse
 
   val app: ZIO[ZConnectionPool & Server, Throwable, Unit] = for {
     conn <- create *> insertRows
--- src/test/scala/ToolsTest.scala
+++ src/test/scala/ToolsTest.scala
@@ -7,15 +7,19 @@ class ToolsTest extends munit.FunSuite {
   val nullResponse = Response.json("""{"response": null}""")
 
   test("csvToList") {
-    val columnName = "date,season,neutral,playoff,team1,team2,elo1_pre,elo2_pre,elo_prob1,elo_prob2,elo1_post,elo2_post,rating1_pre,rating2_pre,pitcher1,pitcher2,pitcher1_rgs,pitcher2_rgs,pitcher1_adj,pitcher2_adj,rating_prob1,rating_prob2,rating1_post,rating2_post,score1,score2".split(",")
+    val columnName =
+      "date,season,neutral,playoff,team1,team2,elo1_pre,elo2_pre,elo_prob1,elo_prob2,elo1_post,elo2_post,rating1_pre,rating2_pre,pitcher1,pitcher2,pitcher1_rgs,pitcher2_rgs,pitcher1_adj,pitcher2_adj,rating_prob1,rating_prob2,rating1_post,rating2_post,score1,score2"
+        .split(",")
     val result: List[List[Option[String]]] = csvToList("./csv/mlb_elo_latest.csv")
 
-    val correctLine: Array[Option[String]] = "2021-10-03,2021,0,,STL,CHC,1519.69110959809,1525.79717673314,0.5257286945873083,0.4742713054126917,,,1510.96575793549,1517.00028701002,,,,,,,0.5273203989501174,0.4726796010498826,,,,"
-    .split(",")
-    .map(value => value match
-        case "" => None
-        case _ => Option(value)
-    )
+    val correctLine: Array[Option[String]] =
+      "2021-10-03,2021,0,,STL,CHC,1519.69110959809,1525.79717673314,0.5257286945873083,0.4742713054126917,,,1510.96575793549,1517.00028701002,,,,,,,0.5273203989501174,0.4726796010498826,,,,"
+        .split(",")
+        .map(value =>
+          value match
+            case "" => None
+            case _  => Option(value)
+        )
 
     assert(result(0).toSeq != columnName.toSeq)
     assertEquals(result(1).toSet, correctLine.toSet)
@@ -37,4 +41,20 @@ class ToolsTest extends munit.FunSuite {
     assertEquals(nullResponse.body, chunkOfTwoToJson(emptyChunkResponse).body)
     assertEquals(expectedResult.body, chunkOfTwoToJson(size2chunkReponse).body)
   }
+
+  test("matchChunkToJsonResponse") {
+    val myMatch = Chunk((0, "2021-10-03", "2021", "ATL", "NYM", 0, 0))
+
+    val expectedResult = Response.json("""[{
+      "id": "0",
+      "date": "2021-10-03",
+      "season": "2021",
+      "team1": "ATL",
+      "team2": "NYM",
+      "score1": 0,
+      "score2": 0
+    }]""")
+    assertEquals(expectedResult.body, matchChunkToJsonReponse(myMatch).body)
+  }
+
 }
